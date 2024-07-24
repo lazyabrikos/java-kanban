b5481fccf9aa269e15cbc196a067a41dab57c97a
vovag <vanovikov_4@edu.hse.ru>
2024-07-24 16:37:10 +0300

    Add duration and startTime, prioritized list

--- src/ru/yandex/javacource/novikov/schedule/tasks/Task.java
+++ src/ru/yandex/javacource/novikov/schedule/tasks/Task.java
@@ -2,7 +2,6 @@ package ru.yandex.javacource.novikov.schedule.tasks;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Task {
